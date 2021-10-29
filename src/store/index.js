83b59daf1c4ac37709c51709f3196c02cd697273
hanshenSun <hanshensunw@gmail.com>
2021-10-29 16:27:56 -0400

    remove vue router

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as fb from '../firebase'
-import router from '../router/index'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
       user: null,
+      loadedModel: null,
       loginUI: fb.ui,
       fbAuth: fb.auth,
       fbStorage: fb.storage,
-      fbDB: fb.db,
-      loadedModel: null
+      fbDB: fb.db      
     },
     mutations: {
       setUserProfile(state, val) {
