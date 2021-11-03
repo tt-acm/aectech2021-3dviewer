df7fa1f50d839d4be317b31349a940c46809dcdc
Hanshen Sun <hanshensunw@gmail.com>
2021-11-03 12:17:36 -0400

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -85,6 +85,27 @@
           <v-divider></v-divider>
         </v-card>
       </v-dialog>
+
+      <!-- Sharing Model Dialog -->
+      <v-dialog v-model="showSharingDialog" width="500">
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            Manage Access
+          </v-card-title>
+
+          <v-card-text v-if="loadedModel">
+            <v-switch v-model="loadedModel.isPublic" label="Make this model public?" ></v-switch>            
+          </v-card-text>
+         
+          <v-progress-linear v-show="modelLoading" color="deep-purple accent-4" indeterminate rounded height="6"></v-progress-linear>
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>            
+            <v-btn color="primary" text @click="saveSharingSettings()"> Save </v-btn>            
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </div>
     
     <!-- UI for Viewer Settings -->
@@ -124,9 +145,9 @@
     <v-row style="margin:10px; position:absolute; left:0px; top:0px; z-index: 12;">
       <v-btn color="primary" @click="showNewModelDialog = true" :disabled="!user"> Upload a New Model </v-btn>
       <v-btn color="secondary" @click="launchModelLoader()" style="margin-left:20px; margin-right:20px" > Load Existing Models </v-btn>
-      <!-- <v-btn class="mx-2" fab dark small color="indigo" @click="showSharingDialog=true" :disabled="!enableShare">
+      <v-btn class="mx-2" fab dark small color="indigo" @click="showSharingDialog=true" :disabled="!enableShare">
         <v-icon dark> mdi-share </v-icon>
-      </v-btn> -->
+      </v-btn>
     </v-row>
   </div>
 </template>
@@ -148,6 +169,7 @@ export default {
       modelUploading: false,
       showNewModelDialog:false,
       showModelLoaderDialog: false,
+      showSharingDialog: false,
       modelLoading: false,
       existingModels: [],
       LoadingTableHeaders:[
@@ -158,6 +180,8 @@ export default {
         { text: '', value: 'loadingAction', sortable: false },
       ],
       itemsPerPage:5,
+      enableShare:false,
+      loadedModel: null
     };
   },
   mounted() {
@@ -209,8 +233,19 @@ export default {
       this.existingModels = [];    
       this.showModelLoaderDialog = true; // Launch model loader popup
 
-      // Get all  models      
-      this.fbDB.collection('models').get().then((querySnapshot) => {
+      if (this.user) {
+        // If logged in, get user's model
+        this.fbDB.collection('models').where('author._id', "==", this.user.uid).get().then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+              const docData = doc.data();
+              docData['_id'] = doc.id;
+              this.existingModels.push(docData);
+          });
+        });   
+      }
+        
+      // Next, get all public models      
+      this.fbDB.collection('models').where("isPublic", "==", true).get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             const docData = doc.data();
             docData['_id'] = doc.id;
@@ -220,34 +255,12 @@ export default {
             }            
         });
       });  
-
-      // if (this.user) {
-      //   // If logged in, get user's model
-      //   this.fbDB.collection('models').where('author._id', "==", this.user.uid).get().then((querySnapshot) => {
-      //     querySnapshot.forEach((doc) => {
-      //         const docData = doc.data();
-      //         docData['_id'] = doc.id;
-      //         this.existingModels.push(docData);
-      //     });
-      //   });   
-      // }
-        
-      // // Next, get all public models      
-      // this.fbDB.collection('models').where("isPublic", "==", true).get().then((querySnapshot) => {
-      //   querySnapshot.forEach((doc) => {
-      //       const docData = doc.data();
-      //       docData['_id'] = doc.id;
-      //       if (this.existingModels.map(m => m._id).indexOf(doc.id) < 0) {
-      //         // Checked for duplicates
-      //         this.existingModels.push(docData);
-      //       }            
-      //   });
-      // });  
     },
     setCurrentModel(model) {
       if (!model) return;
       this.loadModel(model);   
       this.enableShare = true;
+      this.loadedModel = model
     },
     loadModel(model) {
       if (!model) return;
@@ -265,6 +278,12 @@ export default {
       this.showModelLoaderDialog = false;
       this.zoomAll(); //Zoom in after loading
     },
+    saveSharingSettings() {
+      // Update model's public access setting
+      const modelRef = this.fbDB.collection("models").doc(this.loadedModel._id);
+      modelRef.update({'isPublic': this.loadedModel.isPublic})
+      this.showSharingDialog = false;
+    }
   }
 };
 </script>
