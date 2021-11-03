5aa409980253b8225f8220fa7a32b1611e880271
Hanshen Sun <hanshensunw@gmail.com>
2021-11-03 10:58:06 -0400

    Setup file uploader dialog and firebase storage connection.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,6 +3,62 @@
     <div id="threeContainer">
       <Three ref="threeViewer" :gridVisibility="gridVisibility" @loading-complete="loadingCompleted"/>
     </div>
+
+    <div class="text-center"> 
+      <!-- New Model Upload Dialog -->
+      <v-dialog v-model="showNewModelDialog" width="500">
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            Upload a New Model
+          </v-card-title>
+
+          <v-card-text>
+            <v-text-field
+              v-model="newModelName"
+              label="Model Name"
+              prepend-icon="mdi-format-text-variant-outline"
+              required
+            ></v-text-field>
+
+            <!-- File Uploader -->
+            <v-file-input
+              v-model="filesForUpload"
+              placeholder="Upload a model"
+              label="File input"
+              multiple
+              prepend-icon="mdi-paperclip"
+              style="margin-top:25px"
+            >
+              <template v-slot:selection="{ text }">
+                <v-chip
+                  small
+                  label
+                  color="primary"
+                >
+                  {{ text }}
+                </v-chip>
+              </template>
+            </v-file-input>        
+          </v-card-text>
+
+          <v-progress-linear :active="modelUploading" indeterminate color="deep-purple accent-4" ></v-progress-linear>    
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>            
+            <v-btn
+              color="primary"
+              text
+              @click="submitFiles();modelUploading=true"
+              :disabled="!newModelName || filesForUpload.length < 1"
+            >
+              Upload!                       
+            </v-btn>
+            
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
     
     <!-- UI for Viewer Settings -->
     <v-row style="margin:10px; position:absolute; left:0px; bottom:0px">
@@ -36,6 +92,15 @@
         </div>
       </div>
     </v-row>
+
+    <!-- Buttons for uploading/ loading/ sharing models -->
+    <v-row style="margin:10px; position:absolute; left:0px; top:0px; z-index: 12;">
+      <v-btn color="primary" @click="showNewModelDialog = true" :disabled="!user"> Upload a New Model </v-btn>
+      <v-btn color="secondary" @click="launchModelLoader()" style="margin-left:20px; margin-right:20px" > Load Existing Models </v-btn>
+      <!-- <v-btn class="mx-2" fab dark small color="indigo" @click="showSharingDialog=true" :disabled="!enableShare">
+        <v-icon dark> mdi-share </v-icon>
+      </v-btn> -->
+    </v-row>
   </div>
 </template>
 
@@ -50,14 +115,18 @@ export default {
   data() {
     return {
       gridVisibility: true,
-      scaleFactor: 1
+      scaleFactor: 1,
+      filesForUpload: [],
+      newModelName: null,
+      modelUploading: false,
+      showNewModelDialog:false
     };
   },
   mounted() {
     this.$refs.threeViewer.setGridVisibility(this.gridVisibility);
   },
   computed: {
-    ...mapState(['user'])
+    ...mapState(['user', 'fbStorage'])
   },
   methods: {
     loadingCompleted(val) {
@@ -73,7 +142,23 @@ export default {
     },
     updateGridVisibility() {
       this.$refs.threeViewer.setGridVisibility(this.gridVisibility);
-    }
+    },
+    submitFiles() {
+      // Upload model to storage and add a record in db.
+      this.fbStorage.child('models/' +  this.filesForUpload[0].name).put(this.filesForUpload[0])
+      .then(snapshot => {
+          console.log("file uploaded");
+          // File Uploaded, clear out old inputs
+          this.newModelName = null;
+          this.filesForUpload = [];
+          this.modelUploading = false;
+          this.showNewModelDialog = false;
+
+      })
+      .catch((error) => {
+          console.error("Error adding document: ", error);
+      });
+    },
   }
 };
 </script>
